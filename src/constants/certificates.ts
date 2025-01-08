7290c18c42992b109fb69dff8feced8de96df361
ronalmoctz <moctz23@outlook.com>
2025-01-08 12:01:20 -0600

    add icons on certificate

--- src/constants/certificates.ts
+++ src/constants/certificates.ts
@@ -1,3 +1,6 @@
+import CiscoLogo from "../assets/Cisco_logo.webp";
+import NdgLogo from "../assets/ndg_logo_p.webp";
+
 export const certificateData = [
   {
     title: "IoT Fundamentals",
@@ -5,6 +8,7 @@ export const certificateData = [
     date: new Date(2023, 8, 9).toLocaleDateString(),
     description:
       "Utilizar sensores de IdC, creación de diagramas Business Canvas, describir el rol de red y computación en la niebla.",
+    icons: [CiscoLogo],
   },
   {
     title: "Linux Essentials",
@@ -12,6 +16,7 @@ export const certificateData = [
     date: new Date(2023, 12, 31).toLocaleDateString(),
     description:
       "Fundamentos de Linux, comandos básicos, administración de usuarios y permisos, y gestión de archivos.",
+    icons: [CiscoLogo, NdgLogo],
   },
   {
     title: "CCNAv7: Redes Empresariales",
@@ -19,5 +24,6 @@ export const certificateData = [
     date: new Date(2023, 8, 9).toLocaleDateString(),
     description:
       "Conceptos avanzados de redes empresariales, configuración de routers y switches, y resolución de problemas de red.",
+    icons: [CiscoLogo],
   },
 ];
