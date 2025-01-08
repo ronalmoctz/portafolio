42e7e1eee7dc3ca955c5800901163ec2b7200e14
ronalmoctz <moctz23@outlook.com>
2025-01-08 15:06:43 -0600

    real info add

--- src/constants/certificates.ts
+++ src/constants/certificates.ts
@@ -4,7 +4,7 @@ import NdgLogo from "../assets/ndg_logo_p.webp";
 export const certificateData = [
   {
     title: "IoT Fundamentals",
-    image: "../../public/47265670_p0.jpg",
+    image: "../../public/883shots_so.png",
     date: new Date(2023, 8, 9).toLocaleDateString(),
     description:
       "Utilizar sensores de IdC, creación de diagramas Business Canvas, describir el rol de red y computación en la niebla.",
@@ -12,7 +12,7 @@ export const certificateData = [
   },
   {
     title: "Linux Essentials",
-    image: "../../public/47265670_p0.jpg",
+    image: "../../public/965shots_so.png",
     date: new Date(2023, 12, 31).toLocaleDateString(),
     description:
       "Fundamentos de Linux, comandos básicos, administración de usuarios y permisos, y gestión de archivos.",
@@ -20,8 +20,8 @@ export const certificateData = [
   },
   {
     title: "CCNAv7: Redes Empresariales",
-    image: "../../public/47265670_p0.jpg",
-    date: new Date(2023, 8, 9).toLocaleDateString(),
+    image: "../../public/974shots_so.png",
+    date: new Date(2023, 7, 29).toLocaleDateString(),
     description:
       "Conceptos avanzados de redes empresariales, configuración de routers y switches, y resolución de problemas de red.",
     icons: [CiscoLogo],
