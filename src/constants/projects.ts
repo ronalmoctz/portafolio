b740465aa10a0a5b4d6e7d478ba0612fdcc849f6
ronalmoctz <moctz23@outlook.com>
2025-04-17 10:53:23 -0600

    logo remove to label

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -5,12 +5,6 @@ export const project = [
     description:
       "Proyecto universitario, destaca el uso de React, custom componenten, JWT, Cookies de sesion, Axios, Tailwind, Material UI, CSS, NodeJS, NPM, Swagger y Playwright para relizar algunos test basicos.",
     tags: ["Rect", "Javascript", "Tailwind", "MySQL"],
-    tecs: [
-      '../icons/ReactIcon.astro',
-      '../icons/TailwindIcon.astro',
-      '../icons/JavaScriptIcon.astro',
-      '../icons/MySQLIcon.astro',
-    ],
     link: "https://github.com/ronalmoctz/UPFIM-DEV.2",
   },
   {
@@ -19,7 +13,6 @@ export const project = [
     description:
       "Documentacion escencial para la creacion de nuestra DB para el proyecto de SICD, usando diagrama E-R y logica de funcionamiento y normalizacion de tablas.",
     tags: ["MySQL"],
-    tecs: ["../icons/MySQLIcon.astro"],
     link: "",
   },
   {
@@ -28,7 +21,6 @@ export const project = [
     description:
       "Maquetado de aplicacion para automatizar el transporte publico en el Estado de Hidalgo, wireframes accesibles, minimalistas y una UI intuitiva.",
     tags: ["Figma", "UI/UX"],
-    tecs: ["../icons/FigmaIcon.astro"],
     link: "https://www.figma.com/design/DsM3yqFBGh6PpBLLwgBCGx/Hackaton-UI%2FUX?node-id=0-1",
   },
   // {
@@ -46,7 +38,6 @@ export const project = [
     description:
       "Maquetado para Granjas Rodriz, usando Figma, con un enfoque en la usabilidad y la experiencia del usuario. Incluye wireframes y prototipos interactivos.",
     tags: ["Figma", "UI/UX"],
-    tecs: ["../icons/FigmaIcon.astro"],
     link: "https://www.figma.com/design/WiI4cp2OCDrvVOtqEpp74W/granjas_rodri?node-id=0-1&m=dev&t=7BAkrUNIaOnFVv1f-1",
   },
   {
@@ -55,10 +46,6 @@ export const project = [
     description:
       "Desarollo de landing page usando Astro y Tailwind, con un enfoque en la optimizacion de SEO y el rendimiento, ademas de un diseño responsivo y atractivo. Para una PIME de granjas porcinas.",
     tags: ["Astro", "Tailwind"],
-    tecs: [
-      '../icons/AstroIcon.astro',
-      '../icons/TailwindIcon.astro',
-    ],
     link: "https://granjas-rodriz.vercel.app/#home",
   },
 ];
